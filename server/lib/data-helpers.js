3ff774c6336b3809509d446917158142fce3d9e0
Di Wu <CaptainAwesome@Di-Wu-MBP.local>
2018-03-01 15:43:18 -0800

    change the color scheme and now data is persist on mongo

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
@@ -9,19 +8,21 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+      db.collection("tweets").insertOne(newTweet, (err, result) => {
+        console.log(result);
         callback(null, true);
       });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, tweets.sort(sortNewestFirst));
       });
     }
-
   };
 }
