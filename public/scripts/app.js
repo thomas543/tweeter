74302315db614f09f902f932504b4e58afea21b6
Di Wu <CaptainAwesome@Di-Wu-MBP.local>
2018-02-28 17:11:07 -0800

    compose button functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,54 +5,11 @@
  */
 $('#document').ready(function(e) {
   /// DATA
-  const data = [{
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-
-
+  function escape(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
 
   const createNewTweetElement = (tweetObj) => {
 
@@ -70,7 +27,7 @@ $('#document').ready(function(e) {
     const $headerH1 = $("<h1>" + username + "</h1>").addClass("username");
     const $headerP = $("<p>" + handle + "</p>").addClass("shorthand");
     const $headerImg = $("<img>").addClass("icon").attr("src", mdAvatar);
-    const $messageP = $("<p>" + content + "</p>").addClass("message");
+    const $messageP = $("<p>" + escape(content) + "</p>").addClass("message");
     const $footerP = $(`<p data-livestamp=` + time + `></p>`).addClass("timeStamp");
     const $footerUl = $(`<ul><li><i class="fas fa-flag"></i></li>
                         <li><i class="fas fa-retweet"></i></li>
@@ -87,12 +44,44 @@ $('#document').ready(function(e) {
     return $tweet;
   }
 
+
+
   function renderTweets(tweets) {
+    $('#tweets-container').empty();
     tweets.forEach((tweet) => {
       const $newTweet = createNewTweetElement(tweet);
-      $('#tweets-container').append($newTweet);
+      $('#tweets-container').prepend($newTweet);
     });
   }
 
-  renderTweets(data);
+
+
+  const loadTweets = () => {
+    $.get('/tweets').done((tweets) => {
+      renderTweets(tweets);
+    });
+  }
+
+  $('.new-tweet form').on('submit', function(event) {
+    event.preventDefault();
+    const userInput = $(this).find("textArea").val();
+    if (!userInput.trim().length) {
+      alert('you cannot send empty tweet!');
+    } else if (userInput.length > 140) {
+      alert('tweet must be with in 140 characters');
+    } else {
+      const data = { text: userInput };
+      $.post('/tweets', data).done(() => {
+        loadTweets();
+      });
+    }
+  });
+  loadTweets();
+
+  $('#composeButton').on('click', (event) => {
+
+    $('.new-tweet').slideToggle("slow");
+    $('.new-tweet form textArea').focus();
+
+  });
 });
